3140b0218225b61989d63770545c10548a8dab67
Jacob Maniscalco <jmaniscalco9@icloud.com>
2021-07-07 16:31:40 -0400

    Added functionality to retrieve all products from db

--- StoreDL/ProductRepository.cs
+++ StoreDL/ProductRepository.cs
@@ -1,12 +1,13 @@
-
-
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using StoreModels;
 
 namespace StoreDL
 {
     public class ProductRepository : IProductRepository
     {
+        private Entities.jacobmaniscalcoprojectContext _context;
         public bool addCustomer(Product p_product)
         {
             throw new System.NotImplementedException();
@@ -14,7 +15,17 @@ namespace StoreDL
 
         public List<Product> getAllProducts()
         {
-            throw new System.NotImplementedException();
+            return _context.Products.Select(
+                product =>
+                    new StoreModels.Product()
+                    {
+                        ID = product.ProductId,
+                        Name = product.Name,
+                        Description = product.Description,
+                        Price = (double) product.Price,
+                        Category = product.Category
+                    }
+            ).ToList();
         }
 
         public Product getProduct(Product p_product)
